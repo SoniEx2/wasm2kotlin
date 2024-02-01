699325f91178e1344ad3e947d956d03fb8cca1c7
SoniEx2 <endermoneymod@gmail.com>
2024-01-31 22:43:38 -0300

    Attempt to use distcc

--- scripts/TC-s390x.cmake
+++ scripts/TC-s390x.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-set(CMAKE_C_COMPILER s390x-linux-gnu-gcc)
+set(CMAKE_C_COMPILER /opt/bin/distcc_symlinks/s390x-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER s390x-linux-gnu-g++)
 
 set(CMAKE_FIND_ROOT_PATH /usr/s390x-linux-gnu)
