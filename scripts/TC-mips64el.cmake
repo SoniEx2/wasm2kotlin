339a8ec9d10689c8bdd83b2a6823d0cdb19ff2a6
SoniEx2 <endermoneymod@gmail.com>
2025-02-18 13:04:23 -0300

    Test on mips64el

---
+++ scripts/TC-mips64el.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_C_COMPILER /opt/bin/distcc_symlinks/mips64el-linux-gnuabi64-gcc)
+set(CMAKE_CXX_COMPILER mips64el-linux-gnuabi64-g++)
+
+set(CMAKE_FIND_ROOT_PATH /usr/mips64el-linux-gnuabi64)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
